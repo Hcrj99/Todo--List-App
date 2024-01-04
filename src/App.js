13c9d673126f0f4c142c9c2f067624a689ecef50
hcrj99 <hcrj99@hotmail.com>
2024-01-04 10:22:01 -0500

    initial struct TodoItem + components struct

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { TodoItem } from './Components/Todoitem/todoitem';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
+      <TodoItem/>
     </div>
   );
 }
---
+++ src/Components/Todosearch/todosearch.jsx
@@ -0,0 +1,7 @@
+
+function TodoSearch () {
+
+}
+
+
+export { TodoSearch };
