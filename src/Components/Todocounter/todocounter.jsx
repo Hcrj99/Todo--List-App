bfd723fd38ca3d7813752176f80b3e6f63b06427
hcrj99 <hcrj99@hotmail.com>
2024-01-04 13:03:45 -0500

    initial struct css app

--- src/Components/Todocounter/todocounter.jsx
+++ src/Components/Todocounter/todocounter.jsx
@@ -1,12 +1,12 @@
 
 function TodoCounter( { todoCompleted, totalTodo } ) {
 	return (
-		<div>
+		<>
 			<h1>
 			Completed {todoCompleted} / {totalTodo} ToDo's
-		</h1>
-		<progress id='progress' max={totalTodo} value={todoCompleted}>{todoCompleted}</progress>
-		</div>
+			</h1>
+			<progress id='progress' max={totalTodo} value={todoCompleted}>{todoCompleted}</progress>
+		</>
 	);
 }
 
