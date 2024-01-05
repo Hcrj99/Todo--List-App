a599c082cb2a739b0a796ff38819cb6d4f978253
hcrj99 <hcrj99@hotmail.com>
2024-01-05 12:24:39 -0500

    add react icons check + delete

--- src/Components/Todoitem/todoitem.jsx
+++ src/Components/Todoitem/todoitem.jsx
@@ -1,15 +1,18 @@
 import './todoitem.css'
+import { FaFlagCheckered } from 'react-icons/fa';
+import { MdDeleteForever } from "react-icons/md";
+
 
 function TodoItem ( { text, completed , type, onComplete, onDelete } ) {
 	return(
 		<div className='todoitem__container'>
 		<span
 		className={`todoitem__dcompleted ${completed && 'todoitem__completed'}`}
-		onClick={onComplete}>V</span>
+		onClick={onComplete}><FaFlagCheckered /></span>
 		<p className={`todoitem__text ${completed && 'todoitem__text-completed'}`}>{text}</p>
 		<p>{type}</p>
 		<span className='todoitem__delete'
-		onClick={onDelete}>x</span>
+		onClick={onDelete}><MdDeleteForever /></span>
 		</div>
 	);
 }
