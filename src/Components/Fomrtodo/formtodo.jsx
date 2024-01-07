c0da358f456fecfbd9fd33e5574d92e6cb65fae3
hcrj99 <hcrj99@hotmail.com>
2024-01-07 10:58:58 -0500

    add choose type + style css

--- src/Components/Fomrtodo/formtodo.jsx
+++ src/Components/Fomrtodo/formtodo.jsx
@@ -5,6 +5,18 @@ function TodoForm () {
 		<form className='form__create-todo'>
 			<label>Write your new ToDo</label>
 			<textarea placeholder='Write your ToDo'/>
+			<div className='type__container'>
+			<label>Choose type</label>
+				<select className='form__select'>
+				<option value='javascript'>Sports</option>
+					<option value='php'>Health</option>
+					<option value='java'>Family</option>
+					<option value='golang'>Study</option>
+					<option value='python'>Social</option>
+					<option value='C++'>Home</option>
+					<option value='erlang'>Money</option>
+				</select>
+			</div>
 			<div className='buttons__container'>
 			<button
 			className='form__button-cancel'>Cancel</button>
